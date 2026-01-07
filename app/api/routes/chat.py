534eae79fa00697609caeae709194df446ce3106
Vackam <53655740+Vackam@users.noreply.github.com>
2026-01-07 17:24:43 +0900

    feat : route 수정

--- app/api/routes/chat.py
+++ app/api/routes/chat.py
@@ -1,41 +1,29 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends
-from pydantic import BaseModel
 
 from app.services.npc.chat_service import ChatService
 from app.api.dependencies.chat_dependencies import get_chat_service
+from app.models.schemas.chat import SuspectChatRequest, ClueChatRequest, ChatResponse
 
 router = APIRouter(prefix="/chats", tags=["chats"])
-
-
-# 요청 바디 정의
-class SuspectChatRequest(BaseModel):
-    scenario_id: int
-    suspect_id: int
-    user_id: int
-    user_message: str
-
-
-class ClueChatRequest(BaseModel):
-    scenario_id: int
-    clue_id: int
-    user_id: int
-    user_message: str
 
 @router.post("/suspect")
 async def chat_with_suspect(
         request: SuspectChatRequest,
         chat_service: Annotated[ChatService, Depends(get_chat_service)]
 ):
-    """
-    용의자와 대화하는 API
-    유저 id를 통해 db에 접속하여 대화 내용을 가져온 후 그것을 바탕으로 다음 대화를 진행한다.
-    """
-    return await chat_service.suspect_chat(**request.model_dump())
+    answer = await chat_service.suspect_chat(**request.model_dump())
+
+    return ChatResponse(
+        user_message=request.user_message,
+        history=request.history,
+        answer=answer
+    )
+
 
 @router.post("/clue")
-async def analyze_clue(
+async def chat_with_clue(
         request: ClueChatRequest,
         chat_service: Annotated[ChatService, Depends(get_chat_service)]
 ):
@@ -43,4 +31,10 @@ async def analyze_clue(
     경관과 대화하는 API
     유저 id를 통해 db에 접속하여 대화 내용을 가져온 후 그것을 바탕으로 다음 대화를 진행한다.
     """
-    return await chat_service.clue_chat(**request.model_dump())
+    answer = await chat_service.clue_chat(**request.model_dump())
+
+    return ChatResponse(
+        user_message=request.user_message,
+        history=request.history,
+        answer=answer
+    )
--- app/api/routes/scenario.py
+++ app/api/routes/scenario.py
@@ -1,17 +1,9 @@
 from fastapi import APIRouter, HTTPException, Depends
 from typing import Annotated
-from pydantic import BaseModel
 
 from app.services.scenario.scenario_service import ScenarioService
 from app.api.dependencies.scenario_dependencies import get_scenario_service
-
-
-class ScenarioSolve(BaseModel):
-    scenario_id: int
-    culprit_id: list[int]
-    user_id: int
-    user_solution: str
-
+from app.models.schemas.solve import ScenarioSolveRequest, ScenarioSolveResponse
 
 router = APIRouter(prefix="/scenarios", tags=["scenarios"])
 
@@ -30,11 +22,12 @@ async def create_daily_scenario(
         raise HTTPException(status_code=500, detail=str(e))
 
 @router.post("/solve")
-async def solve_scenario(solution: ScenarioSolve):
+async def solve_scenario(solution: ScenarioSolveRequest):
     """
     유저가 범인의 아이디와 정답을 제시합니다.
     solution을 통해 합리적인 제시인 지를 확인하고 정답을 내립니다.
     """
-    return {"message": "Solution submitted (Under construction)",
-            "scenario_id": solution.scenario_id,
-            "success": True}
+    return ScenarioSolveResponse(
+        scenario_id=solution.scenario_id,
+        success=True,
+    )
